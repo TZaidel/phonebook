dba6b8cef316552220895f292becb702772e4b0c
TZaidel <8tzaidel@gmail.com>
2024-03-10 17:54:50 +0100

    edit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import Contact from '../Contact/Contact.jsx';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectUsers, selectNameFilter } from '../redux/selectors.js';
+import { selectUsers, selectNameFilter } from '../../redux/selectors.js';
 import { fetchContacts } from '../../redux/operations.js';
 
 import css from './ContactList.module.css';
